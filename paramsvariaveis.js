f95961f1e9b0cc81c4e531635cfef4e21be6c48c
adri2015 <adriano20142002@gmail.com>
2019-03-04 09:40:30 -0300

    Novo

---
+++ paramsvariaveis.js
@@ -0,0 +1,10 @@
+function soma() {
+    let soma = 1
+    for (let i in arguments) {//Palavra arguments é um array que recebe todas as os paramentos recebidas pela function.
+        soma *= arguments[i]//Essa linha seria a mesma formar que 'soma = soma + arguments[i]'
+    }
+return soma
+}
+console.log(soma(5.5, 5.5))
+console.log(soma(1))
+
